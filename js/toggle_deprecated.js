e97829e1bfd468115fd5a4fc41b9e68e26099c23
Marco Dal Molin <5373628+dalmo1991@users.noreply.github.com>
2021-10-27 08:55:15 +0200

    Update

--- js/toggle_deprecated.js
+++ js/toggle_deprecated.js
@@ -1,22 +1,21 @@
-var button = document.createElement("button")
-button.innerHTML = "Hide deprecated material"
-button.id = "toggle-deprecated"
-button.onclick = function (){
-    
-    var button = document.getElementById("toggle-deprecated")
-    var sections = document.getElementsByClassName('deprecated')
-    
-    if (button.textContent=="Hide deprecated material"){
-        button.textContent = "Show deprecated material";
-        var display = "none";
-    }else{
-        button.textContent = "Hide deprecated material";
-        var display = "block";
-    }
-    
-    for (let index = 0; index < sections.length; index++) {
-        sections[index].style.display = display;
-    }
-}
+var button = document.createElement("button");
+button.innerHTML = "Hide deprecated material";
+button.id = "toggle-deprecated";
+button.onclick = function () {
+  var button = document.getElementById("toggle-deprecated");
+  var sections = document.getElementsByClassName("deprecated");
 
-document.getElementById("doc-title").appendChild(button)
+  if (button.textContent == "Hide deprecated material") {
+    button.textContent = "Show deprecated material";
+    var display = "none";
+  } else {
+    button.textContent = "Hide deprecated material";
+    var display = "block";
+  }
+
+  for (let index = 0; index < sections.length; index++) {
+    sections[index].style.display = display;
+  }
+};
+
+document.getElementsByClassName("index-list")[0].appendChild(button);
